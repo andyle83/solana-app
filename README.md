7a5b0cc05ea1a315b6ddb4e58fddb71c2051b938
Tuan Anh Le <anh.le@myob.com>
2022-04-18 18:23:37 +1000

    feat: Update react configure and template

--- README.md
+++ README.md
@@ -26,3 +26,4 @@ We can use `solana logs` command to view logs output
 
 **References**
 + https://dev.to/edge-and-node/the-complete-guide-to-full-stack-solana-development-with-react-anchor-rust-and-phantom-3291
++ https://github.com/solana-labs/dapp-scaffold/blob/main/package.json
---
+++ app/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = (props) => {
+  return (
+    <div>
+      <Head>
+        <title>Solana Application</title>
+        <meta
+          name="description"
+          content="Solana Scaffold"
+        />
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
