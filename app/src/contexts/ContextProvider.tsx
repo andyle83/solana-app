5343004f401411726eebed4f706680917cfbe4d9
Tuan Anh Le <anh.le@myob.com>
2022-04-24 11:46:05 +1000

    feat: code cleanup for network

--- app/src/contexts/ContextProvider.tsx
+++ app/src/contexts/ContextProvider.tsx
@@ -1,17 +1,15 @@
-import { WalletAdapterNetwork, WalletError } from '@solana/wallet-adapter-base';
+import { WalletError } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider as ReactUIWalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import {PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
 import { FC, ReactNode, useCallback, useMemo } from 'react';
 import { AutoConnectProvider, useAutoConnect } from './AutoConnectProvider';
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const { autoConnect } = useAutoConnect();
-  const network = WalletAdapterNetwork.Devnet;
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const endpoint = 'http://127.0.0.1:8899';
 
-  const wallets = useMemo(() => [ new PhantomWalletAdapter() ], [network]);
+  const wallets = useMemo(() => [ new PhantomWalletAdapter() ], [endpoint]);
 
   const onError = useCallback(
     (error: WalletError) => {
